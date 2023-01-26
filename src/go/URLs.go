cbdffbed488f6ce219dbdfa241db16ae0f32f0da
Hugo Day <day98@purdue.edu>
2023-01-26 14:46:57 -0500

    Go clone working + linked URL list

--- src/go/URLs.go
+++ src/go/URLs.go
@@ -3,9 +3,12 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"github.com/estebangarcia21/subprocess"
+	// "github.com/go-git/go-git/v5"
 	"log"
 	"os"
 	"os/exec"
+	// "subprocess"
 )
 
 type URLnode struct {
@@ -21,11 +24,22 @@ func runModule(function string) string {
 	setup := "import sys; sys.path.append('../'); from src.python import test;"
 	cmd := exec.Command("python", "-c", setup+"print("+function+")")
 	// fmt.Println(cmd.Args)
-	out, err := cmd.CombinedOutput()
+	out, err := cmd.Output()
 	if err != nil {
 		fmt.Println(err)
 	}
 	return string(out)
+}
+
+func cloneRepo(url string) string {
+	cmd := exec.Command("git", "clone ", url)
+	// fmt.Println(cmd.Args)
+	err := cmd.Run()
+	if err != nil {
+		fmt.Println(err)
+		return ("ERROR")
+	}
+	return string("SUCCESS")
 }
 
 func traverseList(next URLnode) {
@@ -57,6 +71,18 @@ func main() {
 	// fmt.Println("Traversing linked list...")
 
 	traverseList(head)
+
+	s := subprocess.New("git clone https://github.com/hugoday/resume")
+
+	if err := s.Exec(); err != nil {
+		log.Fatal(err)
+	}
+
+	url := head.next
+	for url != nil {
+		fmt.Println(cloneRepo(url.url))
+		url = url.next
+	}
 
 	// fmt.Println("[DONE]")
 
