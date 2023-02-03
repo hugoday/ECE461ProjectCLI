8bf081e3994ee97deefffe1c2d725c1ab787737f
Hugo Day <day98@purdue.edu>
2023-02-03 15:18:44 -0500

    Merge go files

--- src/go/Repo.go
+++ src/go/Repo.go
@@ -1,68 +1,68 @@
-package main
+// package main
 
-import "fmt"
+// import "fmt"
 
-// Struct for a repository
-// Includes each metric, and the total score at the end
-// this repo struct will be the input to the linked lists,
-// where we will pass urls by accessing the repo's url
-type repo struct {
-	URL                  string
-	responsiveness       int
-	correctness          int
-	rampUpTime           int
-	busFactor            int
-	licenseCompatibility int
-	totalScore           int
-}
+// // Struct for a repository
+// // Includes each metric, and the total score at the end
+// // this repo struct will be the input to the linked lists,
+// // where we will pass urls by accessing the repo's url
+// type repo struct {
+// 	URL                  string
+// 	responsiveness       int
+// 	correctness          int
+// 	rampUpTime           int
+// 	busFactor            int
+// 	licenseCompatibility int
+// 	totalScore           int
+// }
 
-// this is a function to utilize createing a new repo and initializing each metric within
-func newRepo(url string) *repo {
-	r := repo{URL: url}
-	r.busFactor = getBusFactor(r.URL)
-	r.correctness = getCorrectness(r.URL)
-	r.licenseCompatibility = getLicenseCompatibility(r.URL)
-	r.rampUpTime = getRampUpTime(r.URL)
-	r.responsiveness = getResponsiveness(r.URL)
-	r.totalScore = r.busFactor + r.correctness + r.licenseCompatibility + r.rampUpTime + r.responsiveness
-	return &r
-}
+// // this is a function to utilize createing a new repo and initializing each metric within
+// func newRepo(url string) *repo {
+// 	r := repo{URL: url}
+// 	r.busFactor = getBusFactor(r.URL)
+// 	r.correctness = getCorrectness(r.URL)
+// 	r.licenseCompatibility = getLicenseCompatibility(r.URL)
+// 	r.rampUpTime = getRampUpTime(r.URL)
+// 	r.responsiveness = getResponsiveness(r.URL)
+// 	r.totalScore = r.busFactor + r.correctness + r.licenseCompatibility + r.rampUpTime + r.responsiveness
+// 	return &r
+// }
 
-// Function to get responsiveness metric score
-func getResponsiveness(url string) int {
+// // Function to get responsiveness metric score
+// func getResponsiveness(url string) int {
 
-	return 5
-}
+// 	return 5
+// }
 
-// Function to get correctness metric score
-func getCorrectness(url string) int {
+// // Function to get correctness metric score
+// func getCorrectness(url string) int {
 
-	return 4
-}
+// 	return 4
+// }
 
-// Function to get ramp-up time metric scor
-func getRampUpTime(url string) int {
+// // Function to get ramp-up time metric scor
+// func getRampUpTime(url string) int {
 
-	return 3
+// 	return 3
 
-}
+// }
 
-// Function to get bus factor metric score
-func getBusFactor(url string) int {
+// // Function to get bus factor metric score
+// func getBusFactor(url string) int {
 
-	return 2
+// 	return 2
 
-}
+// }
 
-// Function to get license compatibility metric score
-func getLicenseCompatibility(url string) int {
+// // Function to get license compatibility metric score
+// func getLicenseCompatibility(url string) int {
 
-	return 1
-}
+// 	return 1
+// }
 
-func main() {
-	var r *repo
-	r = newRepo("github.com")
+// func main() {
+// 	var r *repo
+// 	r = newRepo("github.com")
 
-	fmt.Println(r)
-}
+// 	fmt.Println(r)
+// }
--- src/go/URLs.go
+++ src/go/URLs.go
@@ -9,6 +9,11 @@ import (
 	"os"
 	"os/exec"
 	// "subprocess"
+	// "fmt"
+	// "log"
+	// "os"
+	"regexp"
+	"strconv"
 )
 
 type URLnode struct {
@@ -67,7 +72,7 @@ func main() {
 		prev.next = &new
 		prev = &new
 	}
-		
+
 	s := subprocess.New("../../../test1.sh")
 	fmt.Println(s.Exec())
 
@@ -87,4 +92,47 @@ func main() {
 
 	fmt.Println("[DONE]")
 
+}
+
+func getCorrectness() {
+
+	regex, _ := regexp.Compile("\"total_count\": [0-9]+") //Regex for finding count of issues in input file
+	num_regex, _ := regexp.Compile("[0-9]+")              //Regex for parsing count into only integer
+
+	//closed issues
+	data_closed, err1 := os.ReadFile("closed.txt")
+	if err1 != nil {
+		fmt.Println("Did not find closed issues file from api")
+		log.Fatal(err1)
+	}
+	closed_count := regex.FindString(string(data_closed))
+	closed_count = num_regex.FindString(closed_count)
+
+	//open issues
+	data_open, err := os.ReadFile("open.txt")
+	if err != nil {
+		fmt.Println("Did not find open issues file from api")
+		log.Fatal(err)
+	}
+	open_count := regex.FindString(string(data_open))
+	open_count = num_regex.FindString(open_count)
+
+	score := calc_score(open_count, closed_count)
+	fmt.Println(score)
+}
+
+func calc_score(s1 string, s2 string) float64 {
+
+	f1, err := strconv.ParseFloat(s1, 32)
+	if err != nil {
+		fmt.Println("Conversion of s1 to string float didn't work.")
+	}
+	f2, err1 := strconv.ParseFloat(s2, 32)
+	if err1 != nil {
+		fmt.Println("Conversion of s2 to string float didn't work.")
+	}
+
+	f3 := f2 / (f1 + f2)
+
+	return f3
 }
