e5d363862c95faaf1501c751855daf6ec39d8090
Hugo Day <day98@purdue.edu>
2023-02-05 16:29:46 -0500

    Remove token

--- src/python/rest_api.py
+++ src/python/rest_api.py
@@ -10,10 +10,10 @@ def getIssues(url=""):
   #get token from environment variable
   # os.system('curl -i -H https://api.github.com/search/issues?q=repo:'+url+'+type:issue+state:closed >> src/python/issues/closed.txt')
   # os.system('curl -i -H https://api.github.com/search/issues?q=repo:'+url+'+type:issue+state:open >> src/python/issues/open.txt')
-  # os.system('curl -i -H "Authorization: token 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" https://api.github.com/search/issues?q=repo:'+url+'+type:issue+state:closed >> src/python/issues/closed.txt')
-  # os.system('curl -i -H "Authorization: token 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" https://api.github.com/search/issues?q=repo:'+url+'+type:issue+state:open >> src/python/issues/open.txt')
-  os.system('curl -i -H "Authorization: token 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" https://api.github.com/search/issues?q=repo:hugoday/resume+type:issue+state:closed >> src/python/issues/closed.txt')
-  os.system('curl -i -H "Authorization: token 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" https://api.github.com/search/issues?q=repo:hugoday/resume+type:issue+state:open >> src/python/issues/open.txt')
+  # os.system('curl -i -H "Authorization: token [TOKEN]" https://api.github.com/search/issues?q=repo:'+url+'+type:issue+state:closed >> src/python/issues/closed.txt')
+  # os.system('curl -i -H "Authorization: token [TOKEN]" https://api.github.com/search/issues?q=repo:'+url+'+type:issue+state:open >> src/python/issues/open.txt')
+  os.system('curl -i -H "Authorization: token [TOKEN]" https://api.github.com/search/issues?q=repo:hugoday/resume+type:issue+state:closed >> src/python/issues/closed.txt')
+  os.system('curl -i -H "Authorization: token [TOKEN]" https://api.github.com/search/issues?q=repo:hugoday/resume+type:issue+state:open >> src/python/issues/open.txt')
 
 def deleteIssues():
   if os.path.exists('src/python/issues/closed.txt'):
