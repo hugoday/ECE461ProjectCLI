3d34723f473fe9f1bb1501882a7bb16a606bc7fa
hugoday <hugoday511@gmail.com>
2023-02-08 15:34:43 -0500

    Correctness gets token

--- src/go/URLs.go
+++ src/go/URLs.go
@@ -170,12 +170,12 @@ func runRestApi(url string) {
 	}
 	url = url[index+5:]
 
+	token := os.Getenv("GITHUB_TOKEN")
 	code := `import os;
 os.remove('src/python/issues/closed.txt') if os.path.exists('src/python/issues/closed.txt') else "continue";
 os.remove('src/python/issues/open.txt') if os.path.exists('src/python/issues/open.txt') else "continue";
-os.system('curl -i -H "Authorization: token <token> " https://api.github.com/search/issues?q=repo:` + url + `+type:issue+state:closed >> src/python/issues/closed.txt');
-os.system('curl -i -H "Authorization: token <token> " https://api.github.com/search/issues?q=repo:` + url + `+type:issue+state:open >> src/python/issues/open.txt');`
-
+os.system('curl -i -H "Authorization: token ` + token + `" https://api.github.com/search/issues?q=repo:` + url + `+type:issue+state:closed >> src/python/issues/closed.txt');
+os.system('curl -i -H "Authorization: token ` + token + `" https://api.github.com/search/issues?q=repo:` + url + `+type:issue+state:open >> src/python/issues/open.txt');`
 	cmd := exec.Command("python3", "-c", code)
 
 	err := cmd.Run()
