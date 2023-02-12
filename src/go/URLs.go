d510485968264a3db0ef27e36bc54b170ca17621
Benjamin Jefferson Pemberton IV <bpembert@eceprog6.ecn.purdue.edu>
2023-02-11 19:12:05 -0500

    Completed commenting and implementation of correctness and responsiveness. License compatibility implemented. LC, BF, and RU Time all need commenting, BF and RU Time need to be fully implemented still.

--- src/go/URLs.go
+++ src/go/URLs.go
@@ -13,15 +13,8 @@ import (
 	"regexp"
 	"strconv"
 	"strings"
-	// "reflect"
 
 	"github.com/estebangarcia21/subprocess"
-	// "subprocess"
-	// "fmt"
-	// "log"
-	// "os"
-	// "github.com/go-git/go-git/v5"
-	// "time"
 )
 
 // * START OF REPO STRUCTS * \\
@@ -32,7 +25,7 @@ import (
 // where we will pass urls by accessing the repo's url
 type repo struct {
 	URL                  string
-	repoName			 string
+	repoName             string
 	responsiveness       float64
 	correctness          float64
 	rampUpTime           float64
@@ -45,23 +38,19 @@ type repo struct {
 // this is a function to utilize createing a new repo and initializing each metric within
 func newRepo(url string) *repo {
 
-
 	r := repo{URL: url}
-	r.repoName =(cloneRepo(url))
+	r.repoName = (cloneRepo(url))
 	r.busFactor = -1
-	r.correctness = -1
-	r.licenseCompatibility = -1
+	r.correctness = getCorrectness(r.URL)
+	r.licenseCompatibility = getLicenseCompatibility(r.URL)
 	r.rampUpTime = -1
 	r.netScore = -1
+	r.responsiveness = getResponsiveness(r.URL)
 
-	make_shortlog_file("ECE461ProjectCLI")
-	r.busFactor = getBusFactor(r.repoName)
+	//make_shortlog_file("ECE461ProjectCLI")
+	//r.busFactor = getBusFactor(r.repoName)
 
-
-	// r.correctness = getCorrectness(r.URL)
-	// r.licenseCompatibility = getLicenseCompatibility(r.URL)
 	// r.rampUpTime = getRampUpTime(r.URL)
-	// r.responsiveness = getResponsiveness(r.URL)
 	//r.totalScore = r.busFactor + int(r.correctness*20) + r.licenseCompatibility + r.rampUpTime + r.responsiveness
 
 	// s := subprocess.New("rmdir --ignore-fail-on-non-empty " + r.repoName, subprocess.Shell)
@@ -77,15 +66,29 @@ func newRepo(url string) *repo {
 
 // Function to get responsiveness metric score
 func getResponsiveness(url string) float64 {
+	// Variable declarations
 	var command string
-	command = "python3 src/python/API.py \"" + url + "\" >> src/python/responsiveness/score.txt"
+
+	// command to run API.py with a URL passed in, output is stored in score.txt
+	command = "python3 src/python/API.py \"" + url + "\" >> src/metric_scores/responsiveness/score.txt"
+
+	// creates process for command on shell
 	s := subprocess.New(command, subprocess.Shell)
+
+	// executes command
 	s.Exec()
-	file, _ := os.Open("src/python/responsiveness/score.txt")
+
+	// opens score.txt
+	file, _ := os.Open("src/metric_scores/responsiveness/score.txt")
+
+	// create scanner to scan file
 	scanner := bufio.NewScanner(file)
+
 	for scanner.Scan() {
+		// reads string in score txt, converts to float64
 		s := scanner.Text()
 		score, err := strconv.ParseFloat(s, 64)
+		// if error return 0 as score and remove file, otherwise return score
 		if err != nil {
 			removeScores()
 			return 0
@@ -94,14 +97,23 @@ func getResponsiveness(url string) float64 {
 			return score
 		}
 	}
+	// in case of error, removeScores, return 0
 	removeScores()
 	return 0
 }
 
+// This function removes the responsiveness score text file
 func removeScores() {
+	// Variable declarations
 	var command string
-	command = "python3 -c 'import os; os.remove(\"src/python/responsiveness/score.txt\") if os.path.exists(\"src/python/responsiveness/score.txt\") else \"continue\";'"
+
+	// command to run API.py with a URL passed in, output is stored in score.txt
+	command = "python3 -c 'import os; os.remove(\"src/metric_scores/responsiveness/score.txt\") if os.path.exists(\"src/metric_scores/responsiveness/score.txt\") else \"continue\";'"
+
+	// creates process for command on shell
 	r := subprocess.New(command, subprocess.Shell)
+
+	// executes command
 	r.Exec()
 }
 
@@ -147,19 +159,19 @@ func getBusFactor(url string) float64 {
 	}
 
 	arr := strings.Split(string(short_log_raw_data), "\n") // parsing shortlog file by lines
-	
+
 	len_log := len(arr) - 1
-	
-	if len_log < 1{
+
+	if len_log < 1 {
 		fmt.Println("No committers for repo " + url)
 		delete_shortlog_file(url)
 		return 0
 	}
-	
+
 	var num_bus_committers int
-	if len_log < 100{
+	if len_log < 100 {
 		num_bus_committers = 1
-	}else{
+	} else {
 		num_bus_committers = len_log / 100
 	}
 
@@ -172,12 +184,12 @@ func getBusFactor(url string) float64 {
 	for i := 0; i < len_log; i++ {
 		num = regex.FindString(arr[i])
 		num_int, err2 := strconv.Atoi(num)
-		if err2 != nil{
+		if err2 != nil {
 			fmt.Println("Conversion from string to int didn't work (bus factor calc)")
 			log.Fatal(err2)
 		}
 		total += num_int
-		if i < num_bus_committers{
+		if i < num_bus_committers {
 			total_bus_guys += num_int
 		}
 	}
@@ -187,32 +199,31 @@ func getBusFactor(url string) float64 {
 	return metric
 }
 
-
-func make_shortlog_file(url string){
+func make_shortlog_file(url string) {
 	os.Chdir(url)
 
-	cmd := exec.Command("git","shortlog","HEAD","-se", "-n")
+	cmd := exec.Command("git", "shortlog", "HEAD", "-se", "-n")
 	cwd, _ := os.Getwd()
 
 	fmt.Println("dir is " + cwd)
 
-	out,err := cmd.Output()
+	out, err := cmd.Output()
 
-	if err != nil{
+	if err != nil {
 		fmt.Println("Did not find closed issues file from api, invalid url: " + url)
-		// log.Fatal(err)	
+		// log.Fatal(err)
 	}
 
 	os.WriteFile("shortlog.txt", out, 0644)
 
 	os.Chdir("../")
-	
+
 	cwd, _ = os.Getwd()
 	fmt.Println("dir is " + cwd)
 
 }
 
-func delete_shortlog_file(url string){
+func delete_shortlog_file(url string) {
 	var command string
 	command = "rm -f " + url + "/shortlog.txt"
 	s := subprocess.New(command, subprocess.Shell)
@@ -226,40 +237,41 @@ func delete_shortlog_file(url string){
 
 // Function to get correctness metric score
 func getCorrectness(url string) float64 {
-	// fmt.Println("Getting correctness...")
 
+	// runs RestAPI on url
 	runRestApi(url)
 
 	regex, _ := regexp.Compile("\"total_count\": [0-9]+") //Regex for finding count of issues in input file
 	num_regex, _ := regexp.Compile("[0-9]+")              //Regex for parsing count into only integer
 
-	//closed issues
-	data_closed, err1 := os.ReadFile("./src/issues/closed.txt")
+	//gets # of closed issues
+	data_closed, err1 := os.ReadFile("./src/metric_scores/correctness/closed.txt")
 	closed_count := regex.FindString(string(data_closed))
 	closed_count = num_regex.FindString(closed_count)
-	
-	//open issues
-	data_open, err := os.ReadFile("./src/issues/open.txt")
+
+	//gets # of open issues
+	data_open, err := os.ReadFile("./src/metric_scores/correctness/open.txt")
 	open_count := regex.FindString(string(data_open))
 	open_count = num_regex.FindString(open_count)
-	if err != nil || err1 != nil{
+	if err != nil || err1 != nil {
 		fmt.Println("Did not find issues file from api, invalid url: " + url)
-		// log.Fatal(err)
 		return 0
 	}
-	// fmt.Println("Open: " + open_count + "\nClosed: " + closed_count)
-	
+
+	// calculates correctness score
 	score := calc_score(open_count, closed_count)
 	if math.IsNaN(score) {
 		score = 0
 	}
-	// fmt.Println(score)
 
+	// removes files API output files
 	teardownRestApi()
 
+	// returns score
 	return score
 }
 
+// Runs rest api
 func runRestApi(url string) int {
 
 	index := strings.Index(url, ".com/")
@@ -268,39 +280,53 @@ func runRestApi(url string) int {
 		return 1
 	}
 	url = url[index+5:]
-	
-	token := os.Getenv("GITHUB_TOKEN")
-	command := "python3 -c 'import os; os.remove(\"src/issues/closed.txt\") if os.path.exists(\"src/issues/closed.txt\") else \"continue\"; os.remove(\"src/issues/open.txt\") if os.path.exists(\"src/issues/open.txt\") else \"continue\"; os.system(\"curl -i -H \\\"Authorization: token "+token+"\\\" https://api.github.com/search/issues?q=repo:"+url+"+type:issue+state:closed >> src/issues/closed.txt\"); os.system(\"curl -i -H \\\"Authorization: token "+token+"\\\" https://api.github.com/search/issues?q=repo:"+url+"+type:issue+state:open >> src/issues/open.txt\");'"
 
+	// gets env github token
+	token := os.Getenv("GITHUB_TOKEN")
+
+	// command to check if files are in directory, removes them, then runs restapi
+	command := "python3 -c 'import os; os.remove(\"src/metric_scores/correctness/closed.txt\") if os.path.exists(\"src/metric_scores/correctness/closed.txt\") else \"continue\"; os.remove(\"src/metric_scores/correctness/open.txt\") if os.path.exists(\"src/metric_scores/correctness/open.txt\") else \"continue\"; os.system(\"curl -i -H \\\"Authorization: token " + token + "\\\" https://api.github.com/search/issues?q=repo:" + url + "+type:issue+state:closed >> src/metric_scores/correctness/closed.txt\"); os.system(\"curl -i -H \\\"Authorization: token " + token + "\\\" https://api.github.com/search/issues?q=repo:" + url + "+type:issue+state:open >> src/metric_scores/correctness/open.txt\");'"
+
+	// creates subprocess to run on shell
 	r := subprocess.New(command, subprocess.Shell)
+
+	//Executes subprocess
 	r.Exec()
+
 	return 0
 }
 
 func teardownRestApi() {
-	setup := "import sys; sys.path.append('../'); from src.python import rest_api;"
-	cmd := exec.Command("python3", "-c", setup+"rest_api.deleteIssues()")
-	_, err := cmd.Output()
-	if err != nil {
-		fmt.Println(err)
-	}
+	// Variable declarations
+	var command string
+
+	// removes correctness open and closed issue files.
+	command = "python3 -c 'import os; os.remove(\"src/metric_scores/correctness/closed.txt\") if os.path.exists(\"src/metric_scores/correctness/closed.txt\") else \"continue\"; os.remove(\"src/metric_scores/correctness/open.txt\") if os.path.exists(\"src/metric_scores/correctness/open.txt\") else \"continue\";'"
+
+	// creates process for command on shell
+	r := subprocess.New(command, subprocess.Shell)
+
+	// executes command
+	r.Exec()
+
 }
 
+// calculates score for correctness
 func calc_score(s1 string, s2 string) float64 {
-
-	// fmt.Println(s1)
-	// fmt.Println(s2)
+	// converts string to float
 	f1, err := strconv.ParseFloat(s1, 32)
 	if err != nil {
 		fmt.Println("Conversion of s1 to string float didn't work.")
 	}
+	//converts string to float
 	f2, err1 := strconv.ParseFloat(s2, 32)
 	if err1 != nil {
 		fmt.Println("Conversion of s2 to string float didn't work.")
 	}
 
-	f3 := f2 / (f1 + f2)
-
+	// round(20 * closed issues / (open + closed issues)) / 20 = correctness score [0,1]
+	f3 := 20 * f2 / (f1 + f2)
+	f3 = math.Round(f3) / 20
 	return f3
 }
 
@@ -310,20 +336,25 @@ func calc_score(s1 string, s2 string) float64 {
 
 // Function to get license compatibility metric score
 func getLicenseCompatibility(url string) float64 {
-	// fmt.Println("Checking for license... ")
 
-	foundLicense := searchForLicenses("./src/repos/rnd/")
+	// checks to see if license is found
+	foundLicense := searchForLicenses("./src/metric_scores/repos/")
 
+	// returns score based on if license is found
 	if foundLicense {
-		fmt.Println("[LICENSE FOUND]")
+		//fmt.Println("[LICENSE FOUND]")
 		return 1
+	} else {
+		//fmt.Println("[LICENSE NOT FOUND]")
+		return 0
 	}
-	fmt.Println("[LICENSE NOT FOUND]")
-	return 0
 }
 
+// searches directories for license
 func searchForLicenses(folder string) bool {
+	// initialize found as false
 	found := false
+
 	//walk the repo looking for the license
 	err := filepath.Walk(folder, func(path string, info os.FileInfo, err error) error {
 		if found {
@@ -337,6 +368,7 @@ func searchForLicenses(folder string) bool {
 				return filepath.SkipDir
 			}
 		} else {
+			// checks if file has license
 			found = checkFileForLicense(path)
 		}
 		return nil
@@ -344,17 +376,20 @@ func searchForLicenses(folder string) bool {
 
 	//catch errors
 	if err != nil {
-		fmt.Println(err)
+		return false
 	}
 	return found
 }
 
+// searches file for license
 func checkFileForLicense(path string) bool {
+	// license to search for
 	license := "LGPL-2.1"
+
+	// searches for license string
 	file, err := os.Open(path)
 	if err != nil {
-		// fmt.Print("Coudln't open path ")
-		fmt.Println(err)
+		//fmt.Println(err)
 		return false
 	}
 	defer file.Close()
@@ -363,7 +398,7 @@ func checkFileForLicense(path string) bool {
 	for scanner.Scan() {
 		line := scanner.Text()
 		if idx := strings.Index(line, license); idx != -1 {
-			fmt.Println("Found license in file:", path)
+			//fmt.Println("Found license in file:", path)
 			return true
 		}
 	}
@@ -375,8 +410,8 @@ func checkFileForLicense(path string) bool {
 // * START OF REPO CLONING/REMOVING  * \\
 
 func cloneRepo(url string) string {
-	fmt.Println("Cloning Repo")
-	s := subprocess.New("git clone "+url+" src/repos/rnd", subprocess.Shell)
+	//fmt.Println("Cloning Repo")
+	s := subprocess.New("git clone --quiet "+url+" src/metric_scores/repos", subprocess.Shell)
 	if err := s.Exec(); err != nil {
 		log.Fatal(err)
 		fmt.Println(err)
@@ -384,10 +419,10 @@ func cloneRepo(url string) string {
 	}
 	index := strings.Index(url, ".com/")
 	if index == -1 {
-		fmt.Println("No '.com/' found in the string")
+		//fmt.Println("No '.com/' found in the string")
 		return "FAILURE"
 	}
-	
+
 	url = url[index+5:]
 	r, _ := regexp.Compile("/")
 	a := r.Split(url, 2)
@@ -395,7 +430,7 @@ func cloneRepo(url string) string {
 }
 
 func clearRepoFolder() {
-	s := subprocess.New("rm -rf ", subprocess.Arg("src/repos/*"), subprocess.Shell)
+	s := subprocess.New("rm -rf ", subprocess.Arg("src/metric_scores/repos"), subprocess.Shell)
 	s.Exec()
 }
 
--- src/go/main.go
+++ src/go/main.go
@@ -2,26 +2,10 @@ package main
 
 import (
 	"bufio"
-	// "fmt"
-	// "log"
-	// "math"
-	// "utils"
 	"os"
-	// "os/exec"
-	// "path/filepath"
-	// "regexp"
-	// "strconv"
-	// "strings"
-
-	// "github.com/estebangarcia21/subprocess"
-	// "subprocess"
-	// "fmt"
-	// "log"
-	// "os"
-	// "github.com/go-git/go-git/v5"
-	// "time"
 )
 
+// Main
 func main() {
 	// Makes sure repository folder is clear
 	clearRepoFolder()
@@ -35,14 +19,14 @@ func main() {
 	var hold *repo
 	head = &repo{URL: "HEAD"}
 
+	// for each url in the file
 	for scanner.Scan() {
 		//Create new repositories with current URL scanned
 		hold = newRepo(scanner.Text())
+		// Adds repository to Linked List in sorted order by net score
 		head = addRepo(head, head.next, hold)
-		// Add New Repo to linked list
-		// NEEDS TO BE REPLACED WITH SORTING METHOD
 	}
 
-	//
+	// Prints each repository in NDJSON format to stdout (sorted highest to low based off net score)
 	printRepo(head.next)
 }
